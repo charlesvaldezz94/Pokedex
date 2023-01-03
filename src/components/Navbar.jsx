0ae82b0a23433c7dc234b4e65b7afcf0948cb5f1
Charles Valdez <charlesvaldezz94@gmail.com>
2023-01-03 17:38:18 -0600

    final links added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <h2> Kanto Region Pokedex </h2>
     <div className="navbarRight">
       <ul className="navbarLinks">
-        <li> <Link to ="/"> Portfolio </Link> </li>
+        <li> <a target="_blank" href="https://resplendent-melomakarona-1c95e0.netlify.app/"> Portfolio </a> </li>
         <li> <a target="_blank" href="https://www.instagram.com/charlesvaldez_/"> Instagram </a> </li>
         <li> <a target="_blank" href="https://www.linkedin.com/in/charles-valdez-293ba5258/"> LinkedIn </a> </li>
       </ul>
